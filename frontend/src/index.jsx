b518b31af0ac12f5250f3b21139de26961e1cdda
jh9098 <90673222+jh9098@users.noreply.github.com>
2025-04-18 21:43:06 +0900

    Update index.jsx
    Signed-off-by: jh9098 <90673222+jh9098@users.noreply.github.com>

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Result from "./Result";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";          // 선택
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/result" element={<Result />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
